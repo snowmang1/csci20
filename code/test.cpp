88ca94b8a8d84580d21fac55ad841aa719ed1013
Evan Drake <evanmdrake@icloud.com>
2025-11-06 11:50:47 -0800

    creating new csci20
    Signed-off-by: Evan Drake <evanmdrake@icloud.com>

---
+++ code/test.cpp
@@ -0,0 +1,41 @@
+#include<cstdio>
+#include<vector>
+
+using std::vector;
+using std::printf;
+
+void print_queue(vector<int> q) {
+    for(int i=0; i<q.size(); i=i+1) {
+        printf("%d, ", q.at(i));
+    }
+    printf("\n");
+    return ;
+}
+
+vector<int> push_q(vector<int> q, int e) {
+    q.push_back(e);
+    return q;
+}
+
+int pop_q(vector<int> q) {
+    int r = q.at(0);        //save the popped elem
+    q.erase(q.begin());     //pop the element
+    return r;               //return popped elem
+}
+
+int main() {
+    vector<int> q = {};
+
+    q = push_q(q, 10);
+    q = push_q(q, 20);
+    q = push_q(q, 30);
+
+    print_queue(q);
+
+    int e = pop_q(q);
+    printf("popped %d\n", e); // 10 to be popped
+
+    print_queue(q);
+
+    return 0;
+}
---
+++ lec/w4/example.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main() {
+	int x = 1;
+	bool it_doesnt_matter = true;
+	// boolean value
+	std::printf("%i", it_doesnt_matter);
+	// booleans are numbers, huzzah
+	std::printf("%i", x);
+	std::printf("hello world");
+	return 0;
+}
