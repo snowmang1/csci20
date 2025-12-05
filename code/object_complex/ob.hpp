7294bbea91ceaec98149ca027af651b3d76724f2
Evan Drake <31263887+snowmang1@users.noreply.github.com>
2025-12-05 22:06:33 +0000

    modified with comments

--- code/object_complex/ob.hpp
+++ code/object_complex/ob.hpp
@@ -5,16 +5,28 @@
 using std::stack;
 using std::vector;
 
+/*
+
+building the class A which will print a vector using a stack buffer
+in two different methods.
+
+*/
+
+// The class A is for printing a vector using a stack buffer.
 class A {
 private:
+    // buffer stack
     stack<char> st;
 public:
     // constructor
     A();
+
     // loads character vector to be unloaded in st
     void load(vector<char>);
+
     // prints all contents of stack
     void st_print();
+
     // obfuscation
     void vec_print(vector<char>);
 };
