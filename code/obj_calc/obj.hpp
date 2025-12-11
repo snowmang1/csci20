f5f95f3877fbf7f530e0148301dbbca39f67682e
Evan Drake <31263887+snowmang1@users.noreply.github.com>
2025-12-11 21:08:08 +0000

    saved

--- code/obj_calc/obj.hpp
+++ code/obj_calc/obj.hpp
@@ -7,19 +7,21 @@ using std::printf;
 /*
 stack calculator built using a monolithic object.
 
-this means that the object will obfuscate the structure (stack) of evaulation.
+this means that the object will obfuscate the structure (stack) of evaulation. [solved]
+    - stack in private of class
 
 this object represents something like a simple calculator taking numbers and evaulating
 
 - problem: representing numbers and operations [solved]
-    we need an encoding for the numbers and operations of our calculator, lets use a struc and operations of our calculator, lets use a struct.
+    we need an encoding for the numbers and operations of our calculator,
+        lets use a struc and operations of our calculator, lets use a struct.
     our structure houses numbers for eval and characters for operations
     the operations will represented by the following:
     - '+' addition
     - '*' subtraction
     - '^' multiplication
     - '5' division
-    - 'e' evaluation
+    - 'e' evaluation (fail safe for evaluatable number detection)
 
 - problem: what are the basic operations of this calculator [SOLVED]
     binary operators on which two numbers will be given (a,b)
@@ -34,6 +36,10 @@ this object represents something like a simple calculator taking numbers and eva
     - stack of type dicho b/c dicho assist in evaulation operation encoding.
 
 - problem: we need to be able to load this stack (st) in proper order. [UNSOLVED]
+    - get data from user
+        - How: 
+        - Where: Constructor(limits object to single use) or function
+            - take a vector of string and parse, 
 
 - problem: we need a way to evaulate a full stack. [UNSOLVED]
 
@@ -41,15 +47,17 @@ this object represents something like a simple calculator taking numbers and eva
 
 class Calc {
 private:
-    struct dicho {
+    struct clowns {
         int num;
         char op;
     };
-    stack<dicho> st;
+    stack<clowns> st;
     int addition(int, int);
     int subtraction(int, int);
     int multiplication(int, int);
     int division(int, int);
 public:
+    Calc(); // initializes values
+    void german_village_clown_school(vector<?>);
 };
 
